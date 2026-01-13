19a25ed478c783799fea6ccc29ea5bab1de4a90b
Anh Pha <anhphapap0@gmail.com>
2026-01-13 15:19:31 +0700

    fix CROS

--- tech/src/main/java/com/pap/tech/configuration/SecurityConfig.java
+++ tech/src/main/java/com/pap/tech/configuration/SecurityConfig.java
@@ -83,7 +83,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "https://tech-store-frontend-tau.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setExposedHeaders(Arrays.asList("Authorization"));
